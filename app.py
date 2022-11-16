740f18f5860f2768ca115e541a22b183eef956ec
genos <wind740910@gmail.com>
2022-11-16 11:18:10 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name" : "my store",
+        "items":[
+            {
+                "name": "chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores":stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name" : request_data["name"], "items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>")
+def create_item(name):
+    pass
