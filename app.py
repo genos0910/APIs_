9161c9f76a15218f76b6f9fb5c5258bcd149b72e
genos <wind740910@gmail.com>
2022-11-23 18:23:24 +0800

    test

--- app.py
+++ app.py
@@ -26,7 +26,18 @@ def create_store():
     new_store = {"name" : request_data["name"], "items":[]}
     stores.append(new_store)
     return new_store, 201
-
 @app.post("/store/<string:name>")
-def create_item(name):
-    pass
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store, 201
+    return {"message":"store not found"}, 404
+@app.post("/store/<string:name>/item")
+def get_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name":request_data["name"], "price":request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message":"store not found"}, 404
